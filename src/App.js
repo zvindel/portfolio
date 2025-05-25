3fb6953a67ffa4388d8d8c2c698d2171ca758505
zvindel <erlendez@stud.ntnu.no>
2025-05-25 10:34:16 +0200

    feat: trying out animejs-library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { animate } from 'animejs';
 
 function App() {
+  useEffect(() => {
+    animate('polygon', {
+      points: '64 68.64 8.574 100 63.446 67.68 64 4 64.554 67.68 119.426 100',
+      alternate: true,
+      loop: true
+    });
+
+    animate(['#turbulence', '#displacement'], {
+      baseFrequency: .05,
+      scale: 15,
+      alternate: true,
+      loop: true
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundColor: '#252423',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <div style={{ color: 'white', fontWeight: 'bold', marginBottom: '16px' }}>
+        Botvor er en bot
+      </div>
+      <div>
+      <svg width="128" height="128" viewBox="0 0 128 128">
+        <filter id="displacementFilter">
+          <feTurbulence id= "turbulence" type="turbulence" numOctaves="2" baseFrequency="0" result="turbulence"/>
+          <feDisplacementMap id="displacement" in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G"/>
+        </filter>
+        <polygon points="64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96"  fill="#FFFFFF" filter="url(#displacementFilter)"/>
+      </svg>
+      </div>
     </div>
   );
 }
